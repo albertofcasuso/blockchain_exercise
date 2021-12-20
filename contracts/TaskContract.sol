879bbbf66e089f9e5a3bd3495c8b8d22d15ea682
Casuso <hi@casuso.dev>
2021-12-20 12:33:40 +0100

    emision de eventos

--- contracts/TaskContract.sol
+++ contracts/TaskContract.sol
@@ -5,6 +5,18 @@ contract TaskContract {
 
     uint public taskCounter = 0;
 
+    event TaskCreated (
+        uint id,
+        string description,
+        bool completed,
+        uint createdAt
+    );
+
+    event toggleTask(
+        uint id,
+        bool completed
+    );
+
     struct Task {
         uint id;
         string description;
@@ -23,11 +35,13 @@ contract TaskContract {
     function createTask(string memory _description) public {
         tasks[taskCounter] = Task(taskCounter,_description, false, block.timestamp);
         taskCounter ++;
+        emit TaskCreated(taskCounter, _description, false, block.timestamp);
     }
 
     function toggleCompleted(uint _id) public {
         Task memory _task = tasks[_id];
         _task.completed = !_task.completed;
         tasks[_id] = _task;
+        emit toggleTask(_id, _task.completed);
     }
 }
